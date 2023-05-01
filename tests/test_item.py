56242583603c4bddd50d852723a44b9d4e368a53
i-panin-19 <ilypanin@gmail.com>
2023-05-01 22:36:15 +0700

    homework1 is complite

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,13 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+from src.item import Item
+
+obj1 = Item('Шоколад', 89.95, 20)
+
+def test_calculate_total_price():
+    assert obj1.calculate_total_price() == 1799.0
+
+def test_apply_discount():
+    obj1.apply_discount()
+    assert obj1.price * Item.pay_rate == obj1.price
+    assert obj1.price == 89.95
+
